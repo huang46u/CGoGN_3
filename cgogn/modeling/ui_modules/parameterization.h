313c5cb33d0e4a2bde532d91a8adbf7e2471c815
huang qijia <qijiahuang1@gmail.com>
2024-05-29 22:42:46 +0200

    Add floater weight

--- cgogn/modeling/ui_modules/parameterization.h
+++ cgogn/modeling/ui_modules/parameterization.h
@@ -30,10 +30,12 @@
 #include <cgogn/ui/imgui_helpers.h>
 #include <cmath>
 #include <filesystem>
-#include <libacc/bvh_trees_spheres.h>
-#include <libacc/bvh_tree.h>
-#include <libacc/kd_tree.h>
+
+#include <libacc/primitives.h>
 #include <cgogn/rendering/frame_manipulator.h>
+#include <Eigen/Sparse>
+#include <cgogn/geometry/algos/angle.h>
+
 
 #include <GLFW/glfw3.h>
 #include <GL/gl3w.h>
@@ -46,6 +48,111 @@ namespace cgogn
 
 namespace ui
 {
+
+using Vec4 = geometry::Vec4;
+using Vec3 = geometry::Vec3;
+using Vec2 = geometry::Vec2;
+using Scalar = geometry::Scalar;
+
+enum boundary_form
+{
+	BOUNDARY_FORM_CIRCLE,
+	BOUNDARY_FORM_SQUARE
+};
+
+enum parameterization_weight
+{
+	WEIGHT_UNIFORM,
+	WEIGHT_FLOATER
+};
+
+void fix_boundary_vertices(CMap2& m, boundary_form form,
+						   typename mesh_traits<CMap2>::template Attribute<bool>* boundary,
+						   typename mesh_traits<CMap2>::template Attribute<Vec3>* tc_position)
+{
+	CMap2::Edge start_edge;
+	foreach_cell(m, [&](CMap2::Edge e) {
+		if (is_incident_to_boundary(m, e))
+		{
+			start_edge = e;
+			return false;
+		}
+		return true;
+		});
+	Dart b = is_boundary(m, start_edge.dart_) ? start_edge.dart_ : phi2(m, start_edge.dart_);
+	CMap2::Face bf(b);
+	std::vector<CMap2::Vertex> boundary_vertices;
+	foreach_incident_vertex(m, bf, [&](CMap2::Vertex bv) {
+		value<bool>(m, boundary, bv) = true;
+		boundary_vertices.push_back(bv);
+		return true;
+	});
+	switch (form)
+	{
+	case cgogn::ui::BOUNDARY_FORM_CIRCLE: {
+		Scalar step = 2 * M_PI / boundary_vertices.size();
+		for (uint32 idx = 0; idx < boundary_vertices.size(); idx++)
+		{
+			Scalar x = std::cos(idx * step);
+			Scalar y = std::sin(idx * step);
+			value<Vec3>(m, tc_position, boundary_vertices[idx]) = Vec3(x, y, 0.0);
+		}
+	}
+		break;
+	case cgogn::ui::BOUNDARY_FORM_SQUARE: {
+		Scalar x, y;
+		uint32 boundarySize = boundary_vertices.size();
+		uint32 sideLength = boundarySize / 4;
+		for (uint32 i = 0; i < boundarySize; ++i)
+		{
+			uint32 idx = index_of(m, boundary_vertices[i]);
+			if (i < sideLength)
+			{
+				// Bottom edge
+				x = static_cast<Scalar>(i) / (sideLength - 1);
+				y = 0.0;
+			}
+			else if (i < 2 * sideLength)
+			{
+				// Right edge
+				x = 1.0;
+				y = static_cast<Scalar>(i - sideLength) / (sideLength - 1);
+			}
+			else if (i < 3 * sideLength)
+			{
+				// Top edge
+				x = 1.0 - static_cast<Scalar>(i - 2 * sideLength) / (sideLength - 1);
+				y = 1.0;
+			}
+			else
+			{
+				// Left edge
+				x = 0.0;
+				y = 1.0 - static_cast<Scalar>(i - 3 * sideLength) / (sideLength - 1);
+			}
+			value<Vec3>(m, tc_position, boundary_vertices[i]) = Vec3(x-0.5, y-0.5, 0.0);
+		}	
+	}
+		break;
+	default:
+		break;
+	}
+}
+
+Vec3 barycentric_coordinates(const Vec2& a, const Vec2& b, const Vec2& c, const Vec2& p)
+{
+	Vec2 v0 = b - a, v1 = c - a, v2 = p - a;
+	Scalar d00 = v0.dot(v0);
+	Scalar d01 = v0.dot(v1);
+	Scalar d11 = v1.dot(v1);
+	Scalar d20 = v2.dot(v0);
+	Scalar d21 = v2.dot(v1);
+	Scalar denom = d00 * d11 - d01 * d01;
+	Scalar v = (d11 * d20 - d01 * d21) / denom;
+	Scalar w = (d00 * d21 - d01 * d20) / denom;
+	Scalar u = 1.0 - v - w;
+	return Vec3(u, v, 1.0 - u - v);
+}
 template <typename SURFACE>
 class Parameterization : public ViewModule
 {
@@ -57,9 +164,7 @@ class Parameterization : public ViewModule
 	using SEdge = typename mesh_traits<SURFACE>::Edge;
 	using SFace = typename mesh_traits<SURFACE>::Face;
 
-	using Vec4 = geometry::Vec4;
-	using Vec3 = geometry::Vec3;
-	using Scalar = geometry::Scalar;
+
 
 public:
 	Parameterization(const App& app) : ViewModule(app, "Parameterization")
@@ -86,9 +191,10 @@ public:
 		std::shared_ptr<SAttribute<Vec3>> surface_face_color_ = nullptr;
 		std::shared_ptr<SAttribute<Vec3>> surface_vertex_color_ = nullptr;
 		std::shared_ptr<SAttribute<Vec3>> surface_tc_position_ = nullptr;
+		std::shared_ptr<SAttribute<bool>> is_boundary_vertex_= nullptr;
 		std::vector<SEdge> boundary_edge;
-
-
+		boundary_form boundary_form_;
+		parameterization_weight weight_;
 		rendering::FrameManipulator frame_manipulator_;
 		bool show_frame_manipulator_;
 		bool manipulating_frame_;
@@ -105,7 +211,13 @@ public:
 		p.surface_face_color_ = get_or_add_attribute<Vec3, SFace>(*p.surface_, "surface_face_color");
 		p.surface_vertex_color_ = get_or_add_attribute<Vec3, SVertex>(*p.surface_, "surface_vert_color");
 		p.surface_tc_position_ = get_or_add_attribute<Vec3, SVertex>(*p.surface_, "tc_position");
-		
+		p.is_boundary_vertex_ = get_or_add_attribute<bool, SVertex>(*p.surface_, "is_boundary_vertex");
+
+		p.boundary_form_ = BOUNDARY_FORM_CIRCLE;
+		foreach_cell(*p.surface_, [&](SVertex v) {
+			value<bool>(*p.surface_, p.is_boundary_vertex_, v) = false;
+			return true;
+		});
 		foreach_cell(*p.surface_, [&](SEdge v) {
 			value<Vec3>(*p.surface_, p.surface_edge_color_, v) = Vec3(0.0, 0.0, 0.0);
 			return true;
@@ -124,31 +236,149 @@ public:
 	//Only applicable for disk-like surface
 	void tutte_embedding(SurfaceParameter& p)
 	{
-		//Identify bord vertices
-		std::vector<Edge> boundary_edges;
-		foreach_cell(*p.surface_, [&](SEdge e) {
-			if (incident_faces(*p.surface_, e).size()==1)
+		fix_boundary_vertices(*p.surface_, p.boundary_form_, p.is_boundary_vertex_.get(), p.surface_tc_position_.get());
+		typedef Eigen::SparseMatrix<Scalar> SparseMatrix;
+		typedef Eigen::Triplet<Scalar> Triplet;
+		std::vector<Triplet> triplets;
+		uint32 nb_vertices = nb_cells<SVertex>(*p.surface_);
+		SparseMatrix A(nb_vertices, nb_vertices);
+		Eigen::MatrixXd B(nb_vertices, 2);
+		B.setZero();
+		auto geodesic_polar_map_coord = add_attribute<Vec2, SVertex>(*p.surface_, "geodesic_polar_map_coord");
+		foreach_cell(*p.surface_, [&](SVertex v) {
+			uint32 v_index = index_of(*p.surface_, v);
+			if ((*p.is_boundary_vertex_)[v_index])
 			{
-				
-				boundary_edges.push_back(e);
-				
+				Vec3 pos = value<Vec3>(*p.surface_, p.surface_tc_position_, v);
+				B(v_index, 0) = pos.x();
+				B(v_index, 1) = pos.y();
+				triplets.push_back(Triplet(v_index, v_index, 1.0));
 			}
-		
+			else
+			{
+				switch (p.weight_)
+				{
+				case WEIGHT_UNIFORM: {
+					foreach_adjacent_vertex_through_edge(*p.surface_, v, [&](SVertex iv) {
+						uint32 iv_index = index_of(*p.surface_, iv);
+						triplets.push_back(Triplet(v_index, iv_index, -1.0));
+						return true;
+					});
+					triplets.push_back(Triplet(v_index, v_index, degree(*p.surface_, v)));
+				}
+				break;
+				// Derived from "Parametrization and smooth approximation of surface triangulations", Section 6
+				// Shape prevsering parameterization
+				case WEIGHT_FLOATER: {
+					Scalar sum = 0.0;
+					(*geodesic_polar_map_coord)[v_index] = Vec2(0.0, 0.0);
+					std::vector<SVertex> one_ring = adjacent_vertices_through_edge(*p.surface_, v);
+					Scalar angle_sum = geometry::vertex_angle_sum(*p.surface_, p.surface_vertex_position_.get(), v);
+
+					uint32 degree = one_ring.size();
+					// geodesic polar mapping
+					// ||p_{k} - p|| = ||x_{jk}-x_{i}||, angle(p_{k}, p, p_{k+}) = 2*Pi * angle(x_{jk}, x_{i}, x_{jk+1})/angle_sum
+					Scalar sum_angle = 0;
+					for (uint32 i = 0; i < degree; i++)
+					{
+						Vec3 pos = (*p.surface_vertex_position_)[v_index];
+
+						SVertex iv = one_ring[i];
+						uint32 iv_index = index_of(*p.surface_, iv);
+						Vec3 pos_v = (*p.surface_vertex_position_)[iv_index];
+						
+						if (i == 0)
+						{
+							(*geodesic_polar_map_coord)[iv_index] = Vec2((pos_v - pos).norm(), 0);
+						}
+						else
+						{
+							SVertex iv_prev = one_ring[(i - 1)];
+							Vec3 pos_prev = value<Vec3>(*p.surface_, p.surface_vertex_position_, iv_prev);
+							sum_angle += geometry::angle(pos_v - pos, pos_prev - pos);
+							Scalar percent = (sum_angle / angle_sum) * 2 * M_PI;
+							(*geodesic_polar_map_coord)[iv_index] =
+								Vec2(std::cos(percent), std::sin(percent)) * (pos_v - pos).norm();
+						}
+					}
+					if (degree == 3)
+					{
+						SVertex va = one_ring[0];
+						SVertex vb = one_ring[1];
+						SVertex vc = one_ring[2];
+						uint32 va_index = index_of(*p.surface_, va);
+						uint32 vb_index = index_of(*p.surface_, vb);
+						uint32 vc_index = index_of(*p.surface_, vc);
+						Vec2 pos = (*geodesic_polar_map_coord)[v_index];
+						Vec2 pos_a = (*geodesic_polar_map_coord)[va_index];
+						Vec2 pos_b = (*geodesic_polar_map_coord)[vb_index];
+						Vec2 pos_c = (*geodesic_polar_map_coord)[vc_index];
+						Vec3 bary_coord = barycentric_coordinates(pos_a, pos_b, pos_c, pos);
+						triplets.push_back(Triplet(v_index, va_index, bary_coord[0]));
+						triplets.push_back(Triplet(v_index, vb_index, bary_coord[1]));
+						triplets.push_back(Triplet(v_index, vc_index, bary_coord[2]));
+						triplets.push_back(Triplet(v_index, v_index, -bary_coord[0] - bary_coord[1] - bary_coord[2]));
+					}
+					else
+					{
+						Eigen::MatrixXd mu(degree, degree);
+						mu.setZero();
+						for (uint32 l = 0; l < degree; l++)
+						{
+							Vec2 pos = (*geodesic_polar_map_coord)[v_index];
+							SVertex lv = one_ring[l];
+							uint32 lv_index = index_of(*p.surface_, lv);
+							Vec2 pos_lv = (*geodesic_polar_map_coord)[lv_index];
+							for (uint32 k = 0; k < degree; k++)
+							{
+								if (k == l || (k+1)%degree == l)
+									continue;
+								SVertex kv = one_ring[k];
+								SVertex kv_next = one_ring[(k + 1) % degree];
+
+								uint32 kv_index = index_of(*p.surface_, kv);
+								uint32 kv_next_index = index_of(*p.surface_, kv_next);
+								Vec2 pos_kv = (*geodesic_polar_map_coord)[kv_index];
+								Vec2 pos_kv_next = (*geodesic_polar_map_coord)[kv_next_index];
+								Vec3 bary_coord = barycentric_coordinates(pos_lv, pos_kv, pos_kv_next, pos);
+								if (bary_coord[0] > 0 && bary_coord[1] > 0 && bary_coord[2] > 0)
+								{
+									mu(l, l) = bary_coord[0];
+									mu(k, l) = bary_coord[1];
+									mu((k + 1) % degree, l) = bary_coord[2];
+									break;
+								}
+							}
+						}
+						Eigen::VectorXd sum_mu = mu.rowwise().sum();
+						double sum_weight = 0;
+						for (uint32 idx = 0; idx < degree; idx++)
+						{
+							triplets.push_back(Triplet(v_index, index_of(*p.surface_,one_ring[idx]), sum_mu(idx) / degree));
+							sum_weight += sum_mu(idx) / degree;
+						}
+						triplets.push_back(Triplet(v_index, v_index, -sum_weight));
+					}
+				}
+				break;
+				default:
+					break;
+				}
+			}
 			return true;
 		});
-		for (auto e : boundary_edges)
-		{
-			auto& vs = incident_vertices(*p.surface_, e);
-			for (auto v : vs)
-			{
-				value<Vec3>(*p.surface_, p.surface_vertex_color_, v) = Vec3(1.0, 0.0, 0.0);
-			}
-			value<Vec3>(*p.surface_, p.surface_edge_color_, e) = Vec3(1.0, 0.0, 0.0);
-		}
-		surface_provider_->emit_attribute_changed(*p.surface_, p.surface_vertex_color_.get());
-		surface_provider_->emit_attribute_changed(*p.surface_, p.surface_edge_color_.get());
-		surface_provider_->emit_attribute_changed(*p.surface_, p.surface_face_color_.get());
-		//Fix the boundary vertices on a square of size 1
+		A.setFromTriplets(triplets.begin(), triplets.end());
+		Eigen::SparseLU<SparseMatrix> solver;
+		solver.compute(A);
+		Eigen::MatrixXd X = solver.solve(B);
+		foreach_cell(*p.surface_, [&](SVertex v) {
+			uint32 v_index = index_of(*p.surface_, v);
+			value<Vec3>(*p.surface_, p.surface_tc_position_, v) = Vec3(X(v_index, 0), X(v_index, 1), 0.0);
+			return true;
+		});
+		remove_attribute<SVertex>(*p.surface_, geodesic_polar_map_coord.get());
+		surface_provider_->emit_attribute_changed(*p.surface_, p.surface_tc_position_.get());
+		
 
 	}
 
@@ -198,21 +428,6 @@ public:
 
 			return true;
 		}); 
-		/*foreach_cell(*p.surface_, [&](SVertex se) {
-			Vec3 pos = value<Vec3>(*p.surface_, p.surface_vertex_position_, se);
-			Scalar d1 = Vec4(pos.x(), pos.y(), pos.z(), 1).dot(plane);
-			if (std::signbit(d1))
-			{
-				auto& es = incident_edges(*p.surface_, se);
-				for (SEdge& e : es)
-				{
-					value<Vec3>(*p.surface_, p.surface_edge_color_, e) = Vec3(1.0, 0.0, 0.0);
-				}
-			
-			}
-
-			return true;
-		}); */
 		surface_provider_->emit_attribute_changed(*p.surface_, p.surface_vertex_color_.get());
 		surface_provider_->emit_attribute_changed(*p.surface_, p.surface_edge_color_.get());
 		surface_provider_->emit_attribute_changed(*p.surface_, p.surface_face_color_.get());
@@ -352,10 +567,6 @@ protected:
 		if (selected_surface_mesh_){
 		
 			SurfaceParameter& p = surface_parameters_[selected_surface_mesh_];
-			imgui_combo_attribute<SVertex, Vec3>(*selected_surface_mesh_, p.surface_vertex_position_, "Position",
-													   [&](const std::shared_ptr<SAttribute<Vec3>>& attribute) {
-														   p.surface_vertex_position_ = attribute;
-													   });
 			
 			if (ImGui::Button("Parameterization init"))
 			{
@@ -364,9 +575,17 @@ protected:
 			
 			if (p.initialized_)
 			{
-				if (ImGui::Button("Cut mesh")){
+				if (ImGui::Button("Cut mesh"))
+				{
 					cut_mesh(p);
 				}
+				ImGui::RadioButton("Circle", (int*)&p.boundary_form_, BOUNDARY_FORM_CIRCLE);
+				ImGui::SameLine();
+				ImGui::RadioButton("Square", (int*)&p.boundary_form_, BOUNDARY_FORM_SQUARE);
+
+				ImGui::RadioButton("Uniform", (int*)&p.weight_, WEIGHT_UNIFORM);
+				ImGui::SameLine();
+				ImGui::RadioButton("Floater", (int*)&p.weight_, WEIGHT_FLOATER);
 				if (ImGui::Button("Tutte embedding"))
 				{
 					tutte_embedding(p);
